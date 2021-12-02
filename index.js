91367f6b978fb9921888e7d8642f75634e08c8f3
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2021-12-02 17:57:11 +0200

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+const mysql = require("mysql");
 const express = require("express");
 const app = express();
 
 let port = process.env.PORT || 8080;
 
+let config = {
+  host: "mydb.tamk.fi",
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+  connectionLimit: 10,
+};
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  pool.query("SELECT * Customer", (error, results) => {
+    res.send(results);
+  });
 });
 
 const server = app.listen(port, () => {
